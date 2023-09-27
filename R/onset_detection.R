6176e7f09278578bafa983095eff4fe751a2a604
sebsilas <sebsilas@gmail.com>
2023-09-27 09:10:06 +0200

    Initial commit

---
+++ R/onset_detection.R
@@ -0,0 +1,75 @@
+
+
+#' Onset detection
+#'
+#' @param file_name
+#' @param transform_file
+#' @param normalise
+#' @param hidePrint
+#' @param type
+#' @param if_bad_result_return_single_na
+#' @param tidy_fun
+#'
+#' @return
+#' @export
+#'
+#' @examples
+onset_detection <- function(file_name,
+                            transform_file = system.file('transform_files/onset_transform.n3', package = 'vampr'),
+                            normalise = 1L,
+                            hidePrint = TRUE,
+                            type = NULL,
+                            if_bad_result_return_single_na = NULL,
+                            tidy_fun = tidy_onset_detection) {
+
+  op_sys <- get_os()
+
+
+  if(op_sys %in% c("osx", "linux", "windows")) {
+
+    set_vamp_variable(op_sys)
+
+    args <- sonic_annotator_construct_args(transform_file, vamp_cmd = "vamp:qm-vamp-plugins:qm-onsetdetector:onsets", file_name, normalise)
+
+    sa_out <- sonic_annotator_construct_command(args, hidePrint, op_sys)
+
+    if(length(sa_out) == 0) {
+      # res <- py
+      in_handle_null(type, file_name)
+    } else {
+
+      res <- read.csv(text = sa_out, header = FALSE) %>%
+        tibble::as_tibble()
+
+      res <- tidy_fun(res)
+
+      file_name <- res$V1[[1]]
+
+      res <- res %>% dplyr::select(-V1)
+
+      res <- tibble::tibble(file_name, res)
+    }
+
+  } else {
+    warning('OS not supported.')
+  }
+
+
+  # cond <- is.null(res$freq) | all(is.na(res$freq))
+  # if(if_bad_result_return_single_na & cond) {
+  #   res <- NA
+  # }
+  return(res)
+}
+
+tidy_onset_detection <- function(res) {
+  res %>%
+    dplyr::rename(onset = V2)
+}
+
+test_onset_detection <- function() {
+  onset_detection(file_name = system.file('test_audio/test_drum.mp3', package = 'vampr') )
+}
+
+# t <- test_onset_detection()
+
---
+++ R/utils.R
@@ -0,0 +1,137 @@
+
+
+set_vamp_variable <- function(os) {
+
+  if(os == "linux") {
+    set_linux()
+  } else if(os == "osx") {
+    set_osx()
+  } else if(os == "windows") {
+    set_windows()
+  } else {
+    warning("Only Linux or Windows 64 bit is currently supported")
+  }
+
+}
+
+
+
+set_windows <- function() {
+
+  # package library path
+  pkg_path <- system.file('bin/windows64', package = 'pyin')
+
+  # in case the user already has VAMP plugins installed
+
+  vamp_path0 <- system2("echo", args = "$VAMP_PATH")
+
+  # potential library path one
+  vamp_path1 <- homePath <- paste0(fs::path_home(), 'C:\\Program Files\\Vamp Plugins')
+
+  # put all together separated by a colon
+  dirs <- paste(pkg_path, vamp_path0, vamp_path1, sep = ";")
+
+  Sys.setenv(VAMP_PATH = dirs)
+  Sys.getenv("VAMP_PATH")
+}
+
+set_osx <- function() {
+
+  # package library path
+  pkg_path <- system.file('bin/osx', package = 'pyin')
+
+  # in case the user already has VAMP plugins installed
+
+  vamp_path0 <- system2("echo", args = "$VAMP_PATH")
+
+  # potential library path one
+  vamp_path1 <- paste0(fs::path_home(), '/Library/Audio/Plug-Ins/Vamp')
+
+  # potential library path 2
+  vamp_path2 <- '/Library/Audio/Plug-Ins/Vamp'
+
+  # put all together separated by a colon
+  dirs <- paste(pkg_path, vamp_path0, vamp_path1, vamp_path2, sep = ":")
+
+  Sys.setenv(VAMP_PATH = dirs)
+}
+
+
+set_linux <- function() {
+
+  # package library path
+  pkg_path <- system.file('bin/linux64', package = 'pyin')
+
+  # in case the user already has VAMP plugins installed
+
+  vamp_path0 <- system2("echo", args = "$VAMP_PATH")
+
+  # potential library path 1
+  vamp_path1 <- paste0('/usr/local/lib/vamp')
+
+  # put all together separated by a colon
+  dirs <- paste(pkg_path, vamp_path0, vamp_path1, sep = ":")
+
+  Sys.setenv(VAMP_PATH = dirs)
+}
+
+sonic_annotator_construct_args <- function(transform_file, vamp_cmd, file_name, normalise) {
+  if(is.null(transform_file)) {
+    args <- c("-d",
+              vamp_cmd,
+              file_name,
+              "-w",
+              "csv --csv-stdout")
+  } else {
+    args <- c(paste0('-t ', transform_file),
+              file_name,
+              "-w",
+              "csv --csv-stdout")
+  }
+
+  if(normalise == 1) {
+    args <- c(args, "--normalise")
+  }
+  args
+}
+
+
+
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+sonic_annotator_construct_command <- function(args, hidePrint = TRUE, os) {
+
+  if(os == "osx") {
+    cmd <- system.file('bin/osx/sonic-annotator', package = 'vampr')
+  } else if(os == "windows") {
+    cmd <- system.file('bin/windows64/sonic-annotator64.exe', package = 'vampr')
+  } else if(os == "linux") {
+    cmd <- system.file('bin/linux64/sonic-annotator', package = 'vampr')
+  } else {
+    warning('OS not supported.')
+  }
+
+  if(hidePrint) {
+    sa_out <- system2(command = cmd,
+                      args = args,
+                      stdout = TRUE, stderr = FALSE)
+  } else {
+    sa_out <- system2(command = cmd,
+                      args = args,
+                      stdout = TRUE)
+  }
+}
